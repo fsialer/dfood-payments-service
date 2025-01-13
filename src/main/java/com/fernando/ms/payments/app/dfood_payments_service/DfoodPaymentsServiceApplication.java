132904a82d0d9fab5a744811650054d951539505
Fernando <asialer05@hotmail.com>
2025-01-13 10:28:21 -0500

    Proyecto base

---
+++ src/main/java/com/fernando/ms/payments/app/dfood_payments_service/DfoodPaymentsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fernando.ms.payments.app.dfood_payments_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfoodPaymentsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfoodPaymentsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernando/ms/payments/app/dfood_payments_service/DfoodPaymentsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernando.ms.payments.app.dfood_payments_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfoodPaymentsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
